31cf4c53a971bb3aea98e2103b24ad4e051501e0
Giuseppe Menti <mentifg@gmail.com>
2020-05-29 16:27:51 -0300

    Add function to send push notifications

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,32 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const firestore = admin.firestore();
+
+const messaging = admin.messaging();
+
+exports.sendNotificationOnUpdateAppointment = functions.firestore
+  .document('/appointments/{id}')
+  .onUpdate(async (snapshot) => {
+    const changedStatus = snapshot.before.get('status') !== snapshot.after.get('status');
+    const newStatusIsBooked = snapshot.after.get('status') === 'booked';
+
+    if (changedStatus && newStatusIsBooked) {
+      const user = await firestore
+        .collection('users')
+        .doc(snapshot.after.get('userId'))
+        .get();
+
+      const deviceToken = user.get('deviceToken');
+      if (deviceToken) {
+        await messaging.sendToDevice(deviceToken, {
+          notification: {
+            title: 'Sua solicitação foi aprovada!',
+            body: 'Acesse o aplicativo para visualizar os detalhes',
+          },
+        });
+      }
+    }
+  });
